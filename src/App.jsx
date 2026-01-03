510e8e680b3be0aa1db2ff487d285f2e552c9c9e
Frank <temitopeadeoye787898@gmail.com>
2026-01-03 02:19:09 +0100

    Added the navigation button.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Todo from './Pages/TODO/Todo'
 import Calculator from './Pages/Calculator/Calculator'
+import { useEffect, useState } from 'react';
 
 
 function App() {
+  const [locationPath, setLocationPath] = useState('');
+  const loaction = useLocation();
+
+  useEffect(() => {
+    location.pathname === '/' ? setLocationPath('Calculator') : setLocationPath('Todo');
+  }, [loaction]);
+
   return (
     <div>
+      <div>
+        <button className='navigator' onClick={() => {
+          location.pathname === '/' ? window.location.href = '/calculator' : window.location.href = '/';
+        }}>Navigate to {locationPath} Page</button>
+      </div>
       <Routes>
         <Route path='/' element={ <Todo/> }/>
         <Route path='/calculator' element={ <Calculator/> }/>
